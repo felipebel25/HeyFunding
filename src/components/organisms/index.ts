0c81d0af8fb20ccd8cc4db35a00786461dc43acd
Felipe Medina <felipe.medina@samgreencorp.com>
2023-03-26 22:17:56 -0500

    develop/home_bugs

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,2 +1,1 @@
-export { DashboardView } from "./dashboard/DashboardView";
 export { LoginView } from "./login/LoginView";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import { HomeView } from "@/components/organisms/dashboard/DashboardView";
-import Head from "next/head";
-
-
 
 interface Props {
   pokemons: any;
