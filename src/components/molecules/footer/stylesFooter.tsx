be9e72104eaeea329d5be0788521440b8933b60b
Felipe Medina <felipe.medina@samgreencorp.com>
2023-03-27 18:00:58 -0500

    bugfix/arreglos_de_diseno_y_escritura

--- src/components/molecules/footer/stylesFooter.tsx
+++ src/components/molecules/footer/stylesFooter.tsx
@@ -1,23 +1,23 @@
 export const styles = {
     footer: {
-        height: "22%",
-        minHeight:"15rem",
+        height: { xs: "20%", md: "10%" },
         width: "100%",
         display: "flex",
         justifyContent: "space-between",
-        padding: "0% 4%"
+        padding: "4%% 4%",
     },
     image: {
         display: "flex",
         width: { xs: "20%", md: "5%" },
-        height: { xs: "50%", md: "55%" },
+        height: { xs: "45%", md: "100%" },
     },
     logos: {
-        width: { xs: "40%", md: "10%" },
-        height: "57%",
+        width: { xs: "40%", md: "11%" },
+        height: {xs:"48%" , md:"100%"},
         display: "flex",
-        alignItems: "center",
+        alignItems: {xs:"center",md:"center"},
         justifyContent: "space-between",
+        pt:{xs: "0%" , md:"0"},
     },
     sn: {
         width: "auto",
