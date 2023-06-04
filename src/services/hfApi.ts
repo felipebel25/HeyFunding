e5f7b085500b64bd28600c45a82c69a7fdeaa79d
Felipe Medina <felipe.medina@samgreencorp.com>
2023-06-04 02:00:13 -0500

    ready

--- src/services/hfApi.ts
+++ src/services/hfApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const hfApi = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_URL}/api`
+    baseURL: `https://hey-funding.vercel.app/api`
 })
 
 export default hfApi
